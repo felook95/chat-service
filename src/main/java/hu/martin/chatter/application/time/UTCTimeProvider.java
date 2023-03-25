8aea395370d191c0ee229c992bbd1215a832c67a
Martin Marta <mrta.martin@gmail.com>
2023-03-25 09:37:14 +0100

    UTCTimeProvider is a component now.

--- src/main/java/hu/martin/chatter/application/time/UTCTimeProvider.java
+++ src/main/java/hu/martin/chatter/application/time/UTCTimeProvider.java
@@ -1,8 +1,11 @@
 package hu.martin.chatter.application.time;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+@Component
 public class UTCTimeProvider implements TimeProvider {
     @Override
     public LocalDateTime now() {
